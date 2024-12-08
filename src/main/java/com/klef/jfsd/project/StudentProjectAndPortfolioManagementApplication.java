01748d2730f58d2e295dde2b9ca7c439b8fe5024
M.Charishma <2200031593@kluniversity.in>
2024-12-08 06:29:14 +0530

    Portfolyx

---
+++ src/main/java/com/klef/jfsd/project/StudentProjectAndPortfolioManagementApplication.java
@@ -0,0 +1,13 @@
+package com.klef.jfsd.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentProjectAndPortfolioManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentProjectAndPortfolioManagementApplication.class, args);
+	}
+
+}
